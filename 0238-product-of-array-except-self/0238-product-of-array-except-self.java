d3cb77f3ffedeea9a42a8e88b76e9e53e0c8b738
mohdshaquibansari <118913864+mohdshaquibansari@users.noreply.github.com>
2026-07-03 17:15:35 +0530

    Time: 2 ms (94.31%), Space: 72.1 MB (27.48%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,22 +1,24 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+
         int n = nums.length;
         int[] ans = new int[n];
 
-       
-        ans[0] = 1;
+        // store prefix product
+        int prefix = 1;
 
-        for(int i = 1; i < n; i++){
-            ans[i] = ans[i-1] * nums[i-1];
+        for(int i = 0; i < n; i++){
+            ans[i] = prefix;
+            prefix *= nums[i];
         }
 
 
-        
+        // multiply suffix product
         int suffix = 1;
 
-        for(int i = n-2; i >= 0; i--){
-            suffix *= nums[i+1];
+        for(int i = n - 1; i >= 0; i--){
             ans[i] *= suffix;
+            suffix *= nums[i];
         }
 
         return ans;
