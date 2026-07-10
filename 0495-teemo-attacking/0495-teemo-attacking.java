ea6a55210910aaf5589f18549a64a6ce969965da
mohdshaquibansari <118913864+mohdshaquibansari@users.noreply.github.com>
2026-07-10 18:04:14 +0530

    Time: 2 ms (99.27%), Space: 47.8 MB (97.13%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int total = 0;
+        for (int i = 0; i < timeSeries.length - 1; i++) {
+            int gap = timeSeries[i + 1] - timeSeries[i];
+            if (gap < duration) {
+                total += gap;
+            } 
+            else {
+               total += duration;
+            }
+        }
+        total += duration;
+        return total;
+    }
+}
