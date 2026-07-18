c5d305f91d239abb0cf19d47d2e5569c63626145
mohdshaquibansari <118913864+mohdshaquibansari@users.noreply.github.com>
2026-07-18 11:24:44 +0530

    Time: 1 ms (77.68%), Space: 42.5 MB (6.15%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isThree(int n) {
+        int div = 0;
+        for(int i =1 ; i<=n; i++){
+            if(n%i==0){
+                div++;
+            }
+        }
+        if(div==3){
+            return true;
+        }
+        return false;
+    }
+}
